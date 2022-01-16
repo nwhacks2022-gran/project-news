380e34ce700176bc8b2057e95929079fde723023
Alex Hsieh <alexhsieh98@gmail.com>
2022-01-15 16:19:42 -0800

    basic layout

--- frontend/project-news/src/App.js
+++ frontend/project-news/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+
+// TODO: url for our python API
+const apiUrl = `http://localhost:5000/`
+const App = () => {
+
+  const [keywords, setKeywords] = useState([])
+  const [isFetching, setIsFetching] = useState(false)
+  const [beforeDate, setBeforeDate] = useState()
+  const [afterDate, setAfterDate] = useState()
+
+
+  const onSubmit = async () => {
+
+    // attempt to contact API; send parameters
+    // use isFetching to determine loading state
+
+    const params = {
+      keywords,
+      beforeDate,
+      afterDate
+    }
+
+    const customUrl = `${apiUrl}?keywords=${keywords}&beforeDate=${beforeDate}&afterDate=${afterDate}`
+    console.log(customUrl)
+
+    // setIsFetching(true)
+    // const response = await fetch(customUrl)
+    // setIsFetching(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>News 4 U</h1>
+        <div className="input-container">
+          <input
+            id="keyword-text"
+            className="text-input"
+            placeholder="Separate keywords using commas: hockey, baseball, Blue Jays"
+            onChange={e => setKeywords(encodeURIComponent(e.target.value))} />
+          <div className="date-container">
+            <input id="date-before" className="date-picker" type="date" onChange={e => setBeforeDate(e.target.value)} />
+            <input id="date-after" className="date-picker" type="date" onChange={e => setAfterDate(e.target.value)}/>
+          </div>
+          <button id="submit" onClick={onSubmit}>Submit</button>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
