fbe4e6c6781a540bf2727de6242f36c97836b659
gzhang <gordonzhang17@gmail.com>
2022-01-15 16:20:52 -0800

    finish fetching news articles endpoint

---
+++ backend/Article.py
@@ -0,0 +1,41 @@
+class Article(object):
+
+    def __init__(self, author, title, url, source, image, published_at, category):
+        self.author = author,
+        self.title = title,
+        self.url = url,
+        self.source = source,
+        self.image = image,
+        self.published_at = published_at,
+        self.category = category,
+        self.sentiment = 0 #initially it's 0
+
+
+    def article_to_json(article):
+        return {
+            'author': article.author,
+            'title': article.title,
+            'url': article.url,
+            'source': article.source,
+            'image': article.image,
+            'published_date': article.published_date,
+            'category': article.category,
+            'sentiment': article.sentiment
+        }
+
+    def set_sentiment(article, sentiment):
+        article.sentiment = sentiment
+
+
+    #mock data object for development
+#     {
+#    "author":null,
+#    "title":"Daniil Medvedev advances to debut Australian Open final with dominant win over Stefanos Tsitsipas",
+#    "description":"Daniil Medvedev produced an impressive display against world No. 6 Stefanos Tsitsipas to advance to the final of the Australian Open.",
+#    "url":"http://rss.cnn.com/~r/rss/edition_tennis/~3/TAY2kwpOFbs/index.html",
+#    "source":"CNN Tennis",
+#    "image":"https://cdn.cnn.com/cnnnext/dam/assets/210219101515-03-aus-open-0219-daniil-medvedev-super-169.jpg",
+#    "published_at":"2021-04-19T17:05:31+00:00",
+#    "category":"sports",
+#    "sentiment": "0"
+#     }
--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,13 @@
+import os
+import requests
+import urllib.parse
+import json
+from Article import Article
+
 from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -6,10 +15,42 @@ app = Flask(__name__)
 def welcome():
     return "Hello World!"
 
-@app.route('/person/')
-def hello():
-    return jsonify({'name':'Gordon',
-                    'address':'Canada'})
+@app.route('/getarticles')
+def get_news_articles():
+  # request_data = request.data
+  # print('request_data: ', request_data) #not gonna work now since there's no frontend
+  # request_args = request.args
+  # print('request_args: ', request_args) #not gonna work now since there's no frontend
+
+  request_url = 'http://api.mediastack.com/v1/news'
+  access_key = os.getenv('MEDIASTACK_API_ACCESS_KEY')
+  NUMBER_ARTICLES_LIMIT = 25
+  print('access key: ', access_key) 
+  
+  keywords = 'tennis canada' #TODO: not working with mulitple keywords
+  date = '2021-11-12,2021-12-01'
+
+  query = { 
+    'access_key': access_key,
+    'keywords':  keywords,
+    'date': date,
+    'limit': NUMBER_ARTICLES_LIMIT,
+  } 
+
+  encoded_query = urllib.parse.urlencode(query)
+  response = requests.get(request_url, params=encoded_query)
+  parsed_articles = parse_articles(response.json())
+  #now, use this list of articles to make call to scarpe the article
+  return 'Good request!'
+
+def parse_articles(response_json):
+  data = response_json['data']
+  articles_objects = []
+
+  for article in data:
+    article_object = Article(article['author'], article['title'], article['url'],article['source'],article['image'],article['published_at'],article['category'])
+    articles_objects.append(article_object)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
